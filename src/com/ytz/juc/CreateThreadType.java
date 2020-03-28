0ea1b26e93ff7047565941ac6c4be2f48c03e3bf
ytz <yangtz0213@163.com>
2020-03-28 17:12:08 +0800

    first commit

---
+++ src/com/ytz/juc/CreateThreadType.java
@@ -0,0 +1,46 @@
+package com.ytz.juc;
+
+/**
+ * @ClassName: CreateThreadType
+ * @Description: 创建线程的方式
+ *                  继承 Thread 类、实现 Runable 接口。
+ *                  Thread init()方法
+ *                      1.新构造的线程对象是由其 parent 线程来进行空间分配的。
+ *                      2.新线程继承了 parent 线程的 group、是否为 Daemon、优先级 priority、加载资源的 contextClassLoader、
+ *                          可继承的 ThreadLocal。
+ *                      3.parent 线程会分配一个唯一的 ID 来标识这个 child 新线程。
+ * @author: yangtianzeng
+ * @date: 2020/3/28 14:47
+ */
+public class CreateThreadType {
+
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        //线程 start()方法的含义是：当前线程（即 parent 线程）同步告知 Java 虚拟机，只要线程规划器空闲，应立即启动调用 start()方法的线程。
+        //底层源码：通过调用start0()方法启动线程
+        myThread.start();//启动线程，最好为线程起一个名字，方便问题排查
+
+        MyThread2 myThread2 = new MyThread2();
+        Thread thread = new Thread(myThread2);
+        thread.start();
+    }
+
+
+}
+
+//继承 Thread类
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("创建线程。。。Thread。。。。");
+    }
+}
+
+//实现Runnable接口
+class MyThread2 implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("创建线程。。。Runnable。。。。");
+    }
+}
---
+++ src/com/ytz/juc/DeadLockDemo2.java
@@ -0,0 +1,50 @@
+package com.ytz.juc;
+
+import com.sun.corba.se.spi.monitoring.MonitoredObject;
+
+/**
+ * @ClassName: DeadLockDemo2
+ * @Description: . 嵌套管程锁死
+ *                     问题：
+ *                      线程1获得A对象的锁。
+ *                      线程1获得对象B的锁（A对象锁还未释放）。
+ *                      线程1调用B.wait()，从而释放了B对象上的锁，但仍然持有对象A的锁。
+ *                      线程2需要同时持有对象A和对象B的锁，才能向线程1发信号B.notify()。
+ *                      线程2无法获得对象A上的锁，因为对象A上的锁当前正被线程1持有。
+ *                      线程2一直被阻塞，等待线程1释放对象A上的锁。
+ *                      线程1一直阻塞，等待线程2的信号，因此不会释放对象A上的锁。
+ * @author: yangtianzeng
+ * @date: 2020/3/28 14:17
+ */
+public class DeadLockDemo2 {
+
+    protected Object Object = new Object();
+    protected boolean isLocked = false;
+
+    /**
+     * 线程1调用lock()方法，Lock对象锁和monitorObject锁，调用monitorObject.wait()阻塞，但仍然持有Lock对象锁
+     * @throws InterruptedException
+     */
+    public void lock() throws InterruptedException {
+        synchronized (this) {
+            while (isLocked) {
+                synchronized (this.Object) {
+                    this.Object.wait();
+                }
+            }
+            isLocked = true;
+        }
+    }
+
+    /**
+     * 线程2调用unlock()方法解锁时，无法获取Lock对象锁，因为线程1一直持有Lock锁，造成嵌套管程锁死
+     */
+    public void unLock() {
+        synchronized (this) {
+            this.isLocked = false;
+            synchronized (this.Object) {
+                this.Object.notify();
+            }
+        }
+    }
+}
